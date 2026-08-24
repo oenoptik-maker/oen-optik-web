c38ab3da5ce88f3b2b3afed4731fa6402f25fe98
oenoptik-maker <oenoptik@gmail.com>
2026-08-24 22:27:31 +0300

    UTS: admin-content.js postMessage ile sayfaya veri gonder

--- chrome-eklenti/admin-content.js
+++ chrome-eklenti/admin-content.js
@@ -10,27 +10,7 @@
       const urunler = data.utsAktarim.urunler;
       if (!urunler || urunler.length === 0) return;
 
-      function tryAktar() {
-        if (typeof window.adminTabAc === 'function' && typeof window.utsBekleyenUrunleriGoster === 'function') {
-          window.adminTabAc('uts');
-          setTimeout(() => {
-            window.utsBekleyenUrunler = urunler;
-            window.utsBekleyenUrunleriGoster(urunler);
-            if (typeof window.showToast === 'function') {
-              window.showToast(urunler.length + ' ürün UTS sayfasından aktarıldı!', 'success');
-            }
-          }, 300);
-          return true;
-        }
-        return false;
-      }
-
-      if (!tryAktar()) {
-        const interval = setInterval(() => {
-          if (tryAktar()) clearInterval(interval);
-        }, 200);
-        setTimeout(() => clearInterval(interval), 10000);
-      }
+      window.postMessage({ type: 'UTS_URUNLER_AKTAR', urunler: urunler }, '*');
     });
   }
 
